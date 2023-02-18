51d867c9e5e1e00e79f65f718aa3a358324a93f0
Wael Fraj <57875662+waelfraj@users.noreply.github.com>
2023-02-18 11:55:48 +0100

    Update calcul and verif

--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,97 @@ import express, {
   NextFunction,
   Request,
   Response,
- } from "express";
+} from "express";
 const app = express();
 
-
-
-const verif  = (req : Request, res :Response, next: NextFunction) => {
+const verif = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const {
     params: { op, a, b },
   } = req;
-  var x :number = +a;
-  var y :number = +b;
+  var x: number = +a;
+  var y: number = +b;
 
-  if ((!Number.isNaN(x)) && (!Number.isNaN(y))){
-    if (op === "sum"||op === "sous"||op === "mult"||op === "div")
-    next()
-    else{
-    res.send("Error: you must choose sum or sous or mult ou div")
+  if (
+    !Number.isNaN(x) &&
+    !Number.isNaN(y)
+  ) {
+    if (
+      op === "sum" ||
+      op === "sous" ||
+      op === "mult" ||
+      op === "div"
+    )
+      next();
+    else {
+      res.send(
+        "Error: you must choose sum or sous or mult ou div"
+      );
     }
-  }else{
-    res.send("Error: a ou b not number")
+  } else {
+    res.send(
+      "Error: a ou b not number"
+    );
   }
-  
+};
 
+const calcul = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const {
+    params: { op, a, b },
+  } = req;
+  var x: number = +a;
+  var y: number = +b;
+  var s;
 
-}
+  switch (op) {
+    case "sum":
+      res
+      .status(200)
+      .send(
+        `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${x+y}`
+      );
+      break;
+    case "sous":
+      res
+      .status(200)
+      .send(
+        `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${x-y}`
+      );  
+    break;
+    case "mult":
+      res
+      .status(200)
+      .send(
+        `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${x*y}`
+      ); 
+    break;
+    case "div":
+      res
+      .status(200)
+      .send(
+        `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${x/y}`
+      );   
+    break;
+      default:
+        break;
+      }
+    
+};
 
-
-app.get("/:op/:a/:b",verif,(req,res)=> {
-  res.status(200).send(`la resultat de l'operation est egale a`);
-});
-
+app.get("/:op/:a/:b",verif,calcul);
 
 app.listen(3000, () => {
   console.log(
     "Server listening on port 3000"
   );
 });
-
-
 
 /*
 
@@ -50,62 +102,7 @@ app.listen(3000, () => {
 );
 */
 
-
-
-
-
-
 /*
-switch (op) {
-      case "somme":
-        res
-          .status(200)
-          .send(
-            `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${sum(
-              a,
-              b
-            )}`
-          );
-        break;
-      case "soustraction":
-        res
-          .status(200)
-          .send(
-            `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${soustraction(
-              a,
-              b
-            )}`
-          );
-        break;
-      case "multiplication":
-        res
-          .status(200)
-          .send(
-            `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${multiplication(
-              a,
-              b
-            )}`
-          );
-        break;
-      case "division":
-        res
-          .status(200)
-          .send(
-            `la resultat de l'operation ${op} de ${a} et ${b} est egale a ${division(
-              a,
-              b
-            )}`
-          );
-        break;
-      default:
-        res
-          .status(200)
-          .send(
-            `Il faut choisir somme ou aaaa`
-          );
-        break;
-    }
-
 
 
 */
