9b909d151eabc8aef85d1bc8893bcb9d08aadc6f
Wael Fraj <57875662+waelfraj@users.noreply.github.com>
2023-02-18 23:19:31 +0100

    add not found route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const routerMathOp = require("./Controllers/MathControllers");
+const routerNotFound = require("./Controllers/NotFoundController");
 
 app.use("/calcul", routerMathOp);
 
+app.use("/", routerNotFound);
 
 app.listen(3000, () => {
   console.log(
